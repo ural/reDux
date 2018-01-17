8acd66ced58f3fccc0fffeccb5421cc9020b49d0
Max Sevostianov <maxsevostianov@192.168.1.3>
2018-01-17 15:18:41 -0500

    added components dir and HomePage and AboutPage

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render () {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>This app uses React, Redux, ReactRouter and variety of other libraries.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
