f71fbde01e1b3e530824b89d7d133d4ca0cd7e07
Max Sevostianov <maxsevostianov@192.168.1.3>
2018-01-18 18:46:54 -0500

    added course title input field to coursesPage.js

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,36 @@
 import React from 'react';
 
 class CoursesPage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+          course: { title: "" }
+        };
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+    onClickSave() {
+        alert('Saving ' + this.state.course.title);
+    }
+
     render() {
         return (
             <div>
                 <h1>Courses</h1>
+                <h2>Add Courses</h2>
+                <input type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title} />
+                <input type="submit"
+                    value="Save"
+                    onClick={this.onClickSave} className="btn-sm" />
             </div>
         );
     }
