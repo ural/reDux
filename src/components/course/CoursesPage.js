dd9746ccdcb6d8ed309b2f8cc0a54230be938d5b
ural <sevomax@gmail.com>
2018-01-21 17:26:47 -0500

    added api dir with mock apis, cleaned out CoursesPage.js

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -7,20 +7,6 @@ class CoursesPage extends React.Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {
-          course: { title: "" }
-        };
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index) {
@@ -34,13 +20,6 @@ class CoursesPage extends React.Component {
                 <ul className="list-group">
                     {this.props.courses.map(this.courseRow)}
                 </ul>
-                <h2>Add Courses</h2>
-                <input type="text"
-                    onChange={this.onTitleChange}
-                    value={this.state.course.title} />
-                <input type="submit"
-                    value="Save"
-                    onClick={this.onClickSave} className="btn-sm" />
             </div>
         );
     }
