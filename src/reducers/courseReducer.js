d6b0c06f3b4b9f185f451c7eca802985ef8f8ce0
ural <sevomax@gmail.com>
2018-01-22 13:29:44 -0500

    reducers namespacing

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -3,7 +3,7 @@ import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
     switch (action.type) {
-        case types.CREATE_COURSE:
+        case types.LOAD_COURSES_SUCCESS:
             return [...state,
                 Object.assign({}, action.course)
             ];
