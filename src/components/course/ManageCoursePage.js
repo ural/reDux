980117a3b0a6c61ca259d226fb6e8a8c6df0b547
ural <sevomax@gmail.com>
2018-01-23 18:54:02 -0500

    brought in authors

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -15,10 +15,12 @@ class ManageCoursePage extends React.Component {
         };
     }
 
+    
+
     render() {
         return (
             <CourseForm
-                allAuthors={[]}
+                allAuthors={this.props.authors}
                 course={this.state.course}
                 errors={this.state.errors}
             />
@@ -27,14 +29,23 @@ class ManageCoursePage extends React.Component {
 }
 
 ManageCoursePage.propTypes = {
-    course: PropTypes.object.isRequired
-    // actions: PropTypes.object.isRequired
+    course: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
     let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+    const authorsFormattedForDropdown = state.authors.map(author => {
+        return {
+          value: author.id,
+          text: author.firstName + ' ' + author.lastName
+        };
+    });
+
     return {
-        course: course
+        course: course,
+        authors: authorsFormattedForDropdown
     };
 }
 
