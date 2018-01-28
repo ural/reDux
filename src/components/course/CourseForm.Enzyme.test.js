947e88fa4682136069968b9998c6469c38425cfb
ural <sevomax@gmail.com>
2018-01-27 19:38:36 -0500

    playing with toBe() and toEqual() of test files

--- src/components/course/CourseForm.Enzyme.test.js
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -21,12 +21,14 @@ describe('CourseForm via Enzyme', () => {
   it('renders form and h1', () => {
     const wrapper = setup(false);
     expect(wrapper.find('form').length).toBe(1);
-    expect(wrapper.find('h1').text()).toEqual('Manage Course');
+   /* expect(wrapper.find('h1').text()).toEqual('Manage Course');*/
+      expect(wrapper.find('h1').text()).toBe('Manage Course');
   });
 
   it('save button is labeled "Save" when not saving', () => {
     const wrapper = setup(false);
-    expect(wrapper.find('input').props().value).toBe('Save');
+    /*expect(wrapper.find('input').props().value).toBe('Save');*/
+      expect(wrapper.find('input').props().value).toEqual('Save');
   });
 
   it('save button is labeled "Saving..." when saving', () => {
